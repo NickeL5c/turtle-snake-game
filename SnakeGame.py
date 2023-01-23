95eb0daeef79f3246e8d5f7a534c88a2b1dc862e
Nicholas Lloyd <nick409100@hotmail.com>
2023-01-23 16:13:16 -0500

    skips menu, doesn't return to menu after game over

--- SnakeGame.py
+++ SnakeGame.py
@@ -9,11 +9,13 @@ Created on Wed Jan 18 16:28:31 2023
 import turtle
 import random
 
-# define size of game window
+# define unchanging variables
 WIDTH = 1480  # width of game in pixels
 HEIGHT = 800  # height of game in pixels
 DELAY = 100  # milliseconds between screen updates
 FOOD_SIZE = 20  # pixels
+
+
 firstTime = True  # so we know if it is first time or not
 
 # set dictionary of directions for snake
@@ -35,6 +37,7 @@ def bindDirKeys():
     screen.onkey(lambda: setSnakeDir("l",2), "a")
     screen.onkey(lambda: setSnakeDir("d",2), "s")
     screen.onkey(lambda: setSnakeDir("r",2), "d")
+    screen.onkey(lambda: turtle.done(), "Escape")
 
 
 # set snake directions on key strokes calling setSnakeDir
@@ -86,9 +89,25 @@ def clearStamps():
     start.clear()
     food.clear()
 
+def collision(player, newHead):
+    global snake1, snake2, highScore
+
+    if player == 1:
+        if newHead in snake1 or newHead in snake2 or newHead[0] < -WIDTH/2 or newHead[0] > WIDTH/2 \
+            or newHead[1] < -HEIGHT/2 or newHead[1] > HEIGHT/2:
+            if score1 > highScore:
+                highScore = score1
+            return True
+    if player == 2:
+        if newHead in snake2 or newHead in snake1 or newHead[0] < -WIDTH / 2 or newHead[0] > WIDTH/2 \
+            or newHead[1] < -HEIGHT / 2 or newHead[1] > HEIGHT / 2:
+            if score2 > highScore:
+                highScore = score2
+            return True
+
 
 def startWindow():
-    global highScore, firstTime, players
+    global highScore, firstTime
 
     clearTurtles()
     screen.update()
@@ -96,64 +115,105 @@ def startWindow():
     start.goto(0, 100)
 
     if firstTime:
-        firstTime = False
         highScore = 0
+        firstTime = False
     else:
-        if players == 2:
-            if score1 > score2:
-                start.write("Player 1 is the winner!", align="center", font=("Times New Roman", 40, "normal"))
-            if score2 > score1:
-                start.write("Player 2 is the winner!", align="center", font=("Times New Roman", 40, "normal"))
-            if score1 == score2:
-                start.write("It was a tie!", align="center", font=("Times New Roman", 40, "normal"))
+        if score1 > score2:
+            start.write("Player 1 is the winner!", align="center", font=("Times New Roman", 40, "normal"))
+        if score2 > score1:
+            start.write("Player 2 is the winner!", align="center", font=("Times New Roman", 40, "normal"))
+        if score1 == score2:
+            start.write("It was a tie!", align="center", font=("Times New Roman", 40, "normal"))
+
     start.goto(0, -150)
-    start.write(f"Welcome to the Snake Game!\nAre there 1 or 2 players?\n"
+    start.write(f"Welcome to the Snake Game!\nAre there 1 or 2 numPlayers?\n"
                 f"(click '1' for 1P or '2' for 2P)\nHigh Score: {highScore}",
                 align="center", font=("Times New Roman", 40, "normal"))
 
+    
+    
+    screen.onkey(reset1(), "1")
+    screen.onkey(reset2(), "2")
     screen.listen()
-    screen.onkey(lambda: reset(1), "1")
-    screen.onkey(lambda: reset(2), "2")
 
     # if player choose 1, then execute reset function
 
-
-def reset(pChoice):
-    global score1, score2, snake1, snake2, snake1Dir, snake2Dir, foodPos, gameOver1, gameOver2, players
-    players = pChoice
-    # set both players to not game over
+def reset1():
+    global score1, score2, snake1, snake2, snake1Dir, foodPos, gameOver1, gameOver2
+    # set both numPlayers to not game over
 
     gameOver1 = False
-    if players == 2:
-        gameOver2 = False
-    if players == 1:
-        gameOver2 = True
+    gameOver2 = True
 
     # make list of lists to store snake body part positions
-    snake1 = [[0, 0], [20, 0], [40, 0], [60, 0]]
+    snake1 = [[0, 0], [20, 0], [40, 0], [60, 0], [80, 0], [100, 0]]
+    snake2 = [1,2,3,4,5]
+    # draw snakefirst time
 
-    if players == 2:
-        snake2 = [[0, 0], [-20, 0], [-40, 0], [-60, 0]]
+    for segment in snake1:
+        stamper1.goto(segment[0], segment[1])
+        stamper1.stamp()
+    
+    #make snake blink a few times before start
+    #if numPlayers == 1:
+     #   for i in range(5):
+      #      turtle.ontimer(stamper1.stamp, 1000)
+
+
+        #for i in range(5):
+         #   turtle.ontimer(stamper1.stamp, 1000)
+          #  turtle.ontimer(stamper2.stamp, 1000)
+
+    # initial direction set for snake
+    snake1Dir = "u"
+
+    # initial score
+    score1 = 0
+    score2 = 0
+    
+    # initial position of food
+    foodPos = getRandomFoodPos()
+    food.goto(foodPos)
+    gameLoop()
+
+def reset2():
+    global score1, score2, snake1, snake2, snake1Dir, snake2Dir, foodPos, gameOver1, gameOver2
+    # set both numPlayers to not game over
+
+    gameOver1 = False
+    gameOver2 = False
+
+    # make list of lists to store snake body part positions
+    snake1 = [[0, 0], [20, 0], [40, 0], [60, 0], [80, 0], [100, 0]]
+
+    snake2 = [[0, 0], [-20, 0], [-40, 0], [-60, 0], [-80, 0], [-100, 0]]
     # draw snakefirst time
 
     for segment in snake1:
         stamper1.goto(segment[0], segment[1])
         stamper1.stamp()
 
-    if players == 2:
-        for segment in snake2:
+    for segment in snake2:
             stamper2.goto(segment[0], segment[1])
             stamper2.stamp()
+    
+    #make snake blink a few times before start
+    #if numPlayers == 1:
+     #   for i in range(5):
+      #      turtle.ontimer(stamper1.stamp, 1000)
+
+
+        #for i in range(5):
+         #   turtle.ontimer(stamper1.stamp, 1000)
+          #  turtle.ontimer(stamper2.stamp, 1000)
 
     # initial direction set for snake
     snake1Dir = "u"
-    if players == 2:
-        snake2Dir = "d"
+    snake2Dir = "d"
 
     # initial score
     score1 = 0
-    if players == 2:
-        score2 = 0
+    score2 = 0
     
     # initial position of food
     foodPos = getRandomFoodPos()
@@ -168,59 +228,57 @@ def gameLoop():
     screen.update()
     
     # make a new head for snake 1 in correct direction
-    newHead1 = snake1[-1].copy()
-    newHead1[0] += offsets[snake1Dir][0]
-    newHead1[1] += offsets[snake1Dir][1]
+    if not gameOver1:
+        newHead1 = snake1[-1].copy()
+        newHead1[0] += offsets[snake1Dir][0]
+        newHead1[1] += offsets[snake1Dir][1]
+
+        # check collisions with self, other snake or wall
+        if collision(1, newHead1):
+            gameOver1 = True
 
     # make a new head for snake 2 in correct direction
-    if players == 2:
+    if not gameOver2:
         newHead2 = snake2[-1].copy()
         newHead2[0] += offsets[snake2Dir][0]
         newHead2[1] += offsets[snake2Dir][1]
+
+        if collision(2, newHead2):
+            gameOver2 == True
     
-    # check collisions with self or wall
-    if newHead1 in snake1 or newHead1[0] < -WIDTH/2 or newHead1[0] > WIDTH/2\
-        or newHead1[1] < -HEIGHT/2 or newHead1[1] > HEIGHT/2:
-        if score1 > highScore:
-            highScore = score1
-        gameOver1 = True
-    if players == 2 and (newHead2 in snake2 or newHead2[0] < -WIDTH / 2 or newHead2[0] > WIDTH / 2
-    or newHead2[1] < -HEIGHT / 2 or newHead2[1] > HEIGHT / 2):
-        if score2 > highScore:
-            highScore = score2
-        gameOver2 = True
 
     if gameOver1 and gameOver2:
         startWindow()
-       
     else:
         # put new head on snake body
-        snake1.append(newHead1)
-        if players == 2:
+        if not gameOver1:
+            snake1.append(newHead1)
+
+             # check for food collision
+            if not foodCollision(1):
+                snake1.pop(0) # keep snake same length unless eat food
+
+            for segment in snake1:
+                stamper1.goto(segment[0], segment[1])
+                stamper1.stamp()
+
+
+
+
+
+        if not gameOver2:
             snake2.append(newHead2)
         
-        # check for food collision
-        if not foodCollision(1):
-            snake1.pop(0) # keep snake same length unless eat food
-
-        if players == 2:
             if not foodCollision(2):
                 snake2.pop(0)  # keep snake same length unless eat food
 
         # draw the new snake
-        for segment in snake1:
-            stamper1.goto(segment[0], segment[1])
-            stamper1.stamp()
-        if players == 2:
             for segment in snake2:
                 stamper2.goto(segment[0], segment[1])
                 stamper2.stamp()
             
         # refresh the screen with new score
-        if players == 1:
-            screen.title(f"SNAKE GAME! Your Score: {score1}")
-        if players == 2:
-            screen.title(f"SNAKE GAME! P1 Score: {score1}, P2 Score: {score2}")
+        screen.title(f"SNAKE GAME! P1 Score: {score1}, P2 Score: {score2}")
         food.stamp()
         screen.update()
         
@@ -263,10 +321,6 @@ screen.title("SNAKE GAME!")
 screen.bgcolor("black")
 screen.tracer(0)  # turns off auto animation mostly
 
-# event handlers
-screen.listen()
-bindDirKeys()
-
 # create stamper object for snake body parts
 stamper1 = turtle.Turtle()
 stamper1.shape("circle")
@@ -290,6 +344,9 @@ start = turtle.Turtle()
 start.color("white")
 start.penup()
 
+# event handlers
+bindDirKeys()
+screen.listen()
     
 # start game
 startWindow()
